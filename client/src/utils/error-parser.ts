47441c66f0588f30f0d44f588e6cc7be472a137c
Benjamin Marquis <bmarquiscom@gmail.com>
2022-03-04 09:16:18 -0500

    Avoid ramda type error

--- client/src/utils/error-parser.ts
+++ client/src/utils/error-parser.ts
@@ -1,13 +1,23 @@
 import * as R from "ramda";
 import { capitalize } from ".";
 
-const ifErrorIncludeReturnMessage = (includedMessage: string, returnedMessage?: string) => ([(error: Error) => Boolean(error.stack?.includes(includedMessage)), R.always(capitalize(returnedMessage || includedMessage))] as [R.SafePred<Error>, () => string]);
+const ifErrorIncludeReturnMessage = (
+  includedMessage: string,
+  returnedMessage?: string
+) => [
+  (error: Error) => Boolean(error.stack?.includes(includedMessage)),
+  R.always(capitalize(returnedMessage || includedMessage)),
+];
 
-export const dummyErrorParser =  R.cond([
+//@ts-expect-error because Ramda types are c***
+export const dummyErrorParser = R.cond<Error, string>([
   ifErrorIncludeReturnMessage("invalid address"),
   ifErrorIncludeReturnMessage("invalid signature"),
   ifErrorIncludeReturnMessage("Lock time has not expired"),
-  ifErrorIncludeReturnMessage("User denied transaction signature", "Transaction rejected"),
+  ifErrorIncludeReturnMessage(
+    "User denied transaction signature",
+    "Transaction rejected"
+  ),
   ifErrorIncludeReturnMessage("Faucet is dry"),
-  [R.T, R.always("Something went wrong, please contact client services")]
-]) as (error:Error) => string;
+  [R.T, R.always("Something went wrong, please contact client services")],
+]) as (error: Error) => string;
